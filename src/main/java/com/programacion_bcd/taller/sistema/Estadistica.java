6f5ba601fd5c048017c1ea2ae87c8fd4719e064f
RamiroBuenar <ramirobuenar2000@gmail.com>
2021-10-27 11:58:28 -0300

    interface

--- src/main/java/com/programacion_bcd/taller/sistema/Estadistica.java
+++ src/main/java/com/programacion_bcd/taller/sistema/Estadistica.java
@@ -1,4 +1,29 @@
 package com.programacion_bcd.taller.sistema;
 
 public interface Estadistica {
+     /**
+     * Conteo de votantes por distrito.
+     */
+    public void cantVotantes();
+    /**
+     * Conteo de los votos en blanco por distrito.
+     */
+    public void cantVotosBlancos();
+    /**
+     * Conteo de votos por partido en cada distrito.
+     */
+    public void votoPorPartido();
+    /**
+     * Calculara el porcentaje de votacion por distrito respecto al padron del mismo.
+     */
+    public void porcDistrito();
+    /**
+     * Calculara el porcentaje general de votoacion por cada lista, 
+     * incluyendo votos en blanco por cada distrito.
+     */
+    public void porcGralVotoLista();
+    /**
+     * Otras estadisticas que surgan relevantes.
+     */
+    public void otrasEstadisticas();
 }
